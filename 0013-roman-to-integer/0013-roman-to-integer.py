4b46e67f20e1754b2e5ff55959773a5d30dd83e1
Aditi Joshi <aditisjoshi14@gmail.com>
2023-03-27 04:05:46 +0530

    Time: 47 ms (73.68%), Space: 13.9 MB (19.59%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,6 +1,4 @@
-# I can be placed before V (5) and X (10) to make 4 and 9. 
-# X can be placed before L (50) and C (100) to make 40 and 90. 
-# C can be placed before D (500) and M (1000) to make 400 and 900.
+# IV=4, IX=9, XL=40, XC=90, CD = 400, CM=900
 class Solution:
     def romanToInt(self, s: str) -> int:
         roman_to_integer = {
