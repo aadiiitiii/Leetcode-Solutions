cca91358f1a091b8ea07178abcfa66803cc159d8
Aditi Joshi <aditisjoshi14@gmail.com>
2025-11-07 22:41:11 -0800

    Time: 0 ms (100%), Space: 18.9 MB (51.26%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -3,8 +3,8 @@ class Solution:
         prevMap ={} #value:index hashmap
 
         for i, n in enumerate(nums):
-            diff = target - n
-            if diff in prevMap:
-                return [prevMap[diff], i]
-            prevMap[n] = i
+            diff = target - n #difference between target and current number
+            if diff in prevMap: #check if it exists
+                return [prevMap[diff], i] #if it does return (location of that number,current location)
+            prevMap[n] = i #else add to hashmap
         return
